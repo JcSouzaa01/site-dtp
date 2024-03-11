0771c9b39abec69805d8bdf733cf482a57f5c9c8
jC.Souza <143744103+JcSouzaa01@users.noreply.github.com>
2024-03-10 23:18:21 -0300

    Add a aminação suave e bordas no NavBar

---
+++ sitedtp.js
@@ -0,0 +1,10 @@
+// Captura o clique nos links e rola suavemente para a seção correspondente
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
